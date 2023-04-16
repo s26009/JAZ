cd25a0c5d286e7137d79a7ac7762108116fd9d4e
Hleb Skachko <s26009@pjwstk.edu.pl>
2023-04-16 15:21:43 +0200

    implemented POST method

--- src/main/java/pl/pjatk/ska/MyRestController.java
+++ src/main/java/pl/pjatk/ska/MyRestController.java
@@ -1,5 +1,8 @@
 package pl.pjatk.ska;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +10,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/test")
 public class MyRestController {
 
+    @GetMapping("/someValue")
+    ResponseEntity<String> someValue() {
+        return ResponseEntity.ok("someValue");
+    }
+
     @GetMapping("/model")
-    ResponseEntity<Car> hello() {
+    ResponseEntity<Car> model() {
         return ResponseEntity.ok(new Car("Toyota", "Corolla", "2358704", 20.0));
+    }
+
+    @GetMapping("/hello")
+    ResponseEntity<String> print(@RequestParam String message) {
+        return ResponseEntity.ok(message);
+    }
+
+    @PostMapping("/create")
+    ResponseEntity<Car> modelPost(@RequestBody String message) throws JSONException {
+        String make = new JSONObject(message).getString("make");
+        return ResponseEntity.ok(new Car(make, "Corolla", "2358704", 20.0));
     }
 }
